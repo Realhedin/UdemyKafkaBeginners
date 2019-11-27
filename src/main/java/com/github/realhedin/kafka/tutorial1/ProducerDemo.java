c0b020d9400b72184ccd3c2e85bbbbe3224f7d4a
Dmitrii_Korolev <korwork@mail.ru>
2019-11-27 14:06:17 -0500

    adding callback of function return to get info about sending results

--- src/main/java/com/github/realhedin/kafka/tutorial1/ProducerDemo.java
+++ src/main/java/com/github/realhedin/kafka/tutorial1/ProducerDemo.java
@@ -4,12 +4,17 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
 public class ProducerDemo {
 
     public static void main(String[] args) {
+
+
+        Logger logger = LoggerFactory.getLogger(ProducerDemo.class);
 
         String bootstrapServers = "localhost:9092";
 
@@ -23,10 +28,29 @@ public class ProducerDemo {
         KafkaProducer<String,String> kafkaProducer = new KafkaProducer<>(properties);
 
         //create ProducerRecord
-        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("first_topic", "hello kafka from API");
+        ProducerRecord<String, String> producerRecord;
 
-        //send data
-        kafkaProducer.send(producerRecord);
+        for (int i = 0; i < 10; i++) {
+            //fill data in producerRecord
+            producerRecord = new ProducerRecord<>("first_topic", "hello kafka from API " + i);
+            //send data
+            kafkaProducer.send(producerRecord, ((recordMetadata, exception) -> {
+                // executes every time a record is successfully sent or an exception is thrown
+                if (exception == null) {
+                    // the record was successfully sent
+                    logger.info("Received new metadata. \n" +
+                            "Topic:" + recordMetadata.topic() + "\n" +
+                            "Partition: " + recordMetadata.partition() + "\n" +
+                            "Offset: " + recordMetadata.offset() + "\n" +
+                            "Timestamp: " + recordMetadata.timestamp());
+                } else {
+                    logger.error("Error while producing", exception);
+                }
+            }));
+        }
+
+        //to force wait until sent
         kafkaProducer.flush();
+        kafkaProducer.close();
     }
 }
